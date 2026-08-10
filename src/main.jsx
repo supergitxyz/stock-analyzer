80ebbddbbe43643ec279632e81b86f962daea62a
supergitxyz <amitgupta.nine@gmail.com>
2026-08-09 22:47:10 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StockAnalyzer from "./StockAnalyzer.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StockAnalyzer />
+  </React.StrictMode>
+);
